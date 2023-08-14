922425751aa3c12badfb7f12abf3d3c063a17efc
Gagema <gabrielgmazolli@gmail.com>
2023-08-14 19:48:15 -0300

    Calculadora

---
+++ calculadora.c
@@ -0,0 +1,38 @@
+#include<stdio.h> 
+#include <stdlib.h>
+int main(){
+int resp=0;
+do{
+    do{printf("BEM VINDO A CALCULADORA!/n");
+    printf("Escolha a operação desejada:\n");
+    printf("1-soma\n");
+    printf("2-subtração\n");
+    printf("3-multiplicação\n");
+    printf("4-divisão\n");
+    scanf("%d",&resp);
+    if ( resp<1 || resp>5){
+        printf("Tente novamente\n");
+        system("pause");
+        system("cls");
+    };
+    system("cls");
+    }while(resp>5 || resp<1);
+
+    switch(resp){
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+        case 4:
+        break;
+        case 5:
+        break;
+    }
+}while(resp!=5);
+if (resp==5){
+    printf("Até mais!");
+    return 0;
+}
+}
